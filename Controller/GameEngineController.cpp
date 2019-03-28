4efc401304daa0ad6e9fbce36a55f7aad7473ce5
SAarronB <43042628+SAarronB@users.noreply.github.com>
2019-03-27 21:32:05 -0600

    Initial commit

---
+++ Controller/GameEngineController.cpp
@@ -0,0 +1,9 @@
+#include "GameEngineController.hpp"
+
+void GameController :: start(){
+    cout << "Welcome to The Game Engine!!" << endl;
+}
+public static void main(){
+    start();
+    
+};
---
+++ Controller/GameEngineController.hpp
@@ -0,0 +1,16 @@
+#ifndef GameEngineController_hpp
+#define GameEngineController_hpp
+
+
+
+#include <iostream>
+#include ""
+#include ""
+
+using namespace std;
+class GameEngineController{
+    private:
+        
+    public:
+        void start();
+}
